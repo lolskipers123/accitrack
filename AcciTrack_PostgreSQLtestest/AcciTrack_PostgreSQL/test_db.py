7cebb4ba65fb03e928b07bacaff0c1d604fde5e8
lolskipers123 <lolskipers1@gmail.com>
2026-06-30 22:06:34 +0800

    Delete AcciTrack_PostgreSQLtestest/AcciTrack_PostgreSQL directory

--- AcciTrack_PostgreSQLtestest/AcciTrack_PostgreSQL/test_db.py
+++
@@ -1,6 +0,0 @@
-from easysql_pg import EasySQL
-
-
-db = EasySQL()
-print(db.get_table_values("AcciTrack", "AcciTrack_ReportList"))
-print(len(db.get_table_values("AcciTrack", "AcciTrack_ReportList")))
